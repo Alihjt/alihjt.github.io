dcc54ac7a1eaefeeac694f0df9ccc7725bea1e65
“aho” <aho@informatik.uni-kiel.de>
2026-02-21 17:31:24 +0100

    fixing typos

--- _make_favicon.sh
+++ _make_favicon.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Generate favicon from the same profile image used on the homepage.
-ffmpeg -y -i "images/prof.jpeg" -vf 'crop=min(iw\,ih):min(iw\,ih),scale=64:64' "favicon.ico"
+# Generate favicon from portrait.jpg.
+ffmpeg -y -i "images/portrait.jpg" -vf 'crop=min(iw\,ih):min(iw\,ih),scale=64:64' "favicon.ico"
